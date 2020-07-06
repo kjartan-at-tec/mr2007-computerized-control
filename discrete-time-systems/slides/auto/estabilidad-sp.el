670eee3078cb89930c9645b74c4a2a097df4edee
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-06 12:30:14 -0500

    Prepared class on block diagrams

---
+++ discrete-time-systems/slides/auto/estabilidad-sp.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "estabilidad-sp"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "aspectratio=169")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "khpreamble"
+    "tcolorbox")
+   (TeX-add-symbols
+    "shift"
+    "diff")
+   (LaTeX-add-labels
+    "sec:orgf3e95ba"
+    "sec:orgd3d7932"))
+ :latex)
+
