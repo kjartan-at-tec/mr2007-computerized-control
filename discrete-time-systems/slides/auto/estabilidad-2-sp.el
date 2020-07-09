c8ae852dda6ca3f7814f7e286600aa3f6c31725c
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-09 13:00:43 -0500

    Fixed typo

--- discrete-time-systems/slides/auto/estabilidad-2-sp.el
+++ discrete-time-systems/slides/auto/estabilidad-2-sp.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,13 +33,10 @@
     "hyperref"
     "khpreamble"
     "tcolorbox")
-   (TeX-add-symbols
-    "shift"
-    "diff")
    (LaTeX-add-labels
-    "sec:org802bb35"
-    "sec:org477f214"
-    "sec:org96d972a"
-    "sec:org6e2371e"))
+    "sec:org9fd54d6"
+    "sec:org9618a6e"
+    "sec:org2400118"
+    "sec:org41b841e"))
  :latex)
 
