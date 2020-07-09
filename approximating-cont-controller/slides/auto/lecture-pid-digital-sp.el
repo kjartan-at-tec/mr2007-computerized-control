c6d4dd14e57d5d88cb479db35a0446b033597b8e
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-09 11:55:46 -0500

    Material for PID lecture

--- approximating-cont-controller/slides/auto/lecture-pid-digital-sp.el
+++ approximating-cont-controller/slides/auto/lecture-pid-digital-sp.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -36,11 +36,17 @@
     "shift"
     "diff"
     "Td"
-    "NN")
+    "NN"
+    "uampl"
+    "ttdelay"
+    "TTcnst"
+    "ggain"
+    "tdelay"
+    "Tcnst")
    (LaTeX-add-labels
-    "sec:org2b2ce81"
-    "sec:orgaa0cd57"
-    "sec:orgb7da7c5"
-    "sec:org396ddcc"))
+    "sec:org39380f2"
+    "sec:org5a7a827"
+    "sec:org950e38a"
+    "sec:org935e007"))
  :latex)
 
---
+++ figures/auto/bode-bessel-unit-delay.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "bode-bessel-unit-delay"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "fontenc"
+    "inputenc"
+    "pgf"
+    "tikz"
+    "setspace"
+    "pgfplots")
+   (TeX-add-symbols
+    "Bessel"))
+ :latex)
+
