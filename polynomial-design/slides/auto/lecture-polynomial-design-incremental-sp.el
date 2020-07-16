07ef8e829016231bccabafb43db946c2db0b0a97
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-16 12:55:21 -0500

    Material for incremental control

--- polynomial-design/slides/auto/lecture-polynomial-design-incremental-sp.el
+++ polynomial-design/slides/auto/lecture-polynomial-design-incremental-sp.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -36,8 +36,10 @@
     "shift"
     "diff")
    (LaTeX-add-labels
-    "sec:org4b702ec"
-    "sec:org6e0467a"
-    "sec:orgdfdc016"))
+    "sec:org1cc0c22"
+    "sec:orgf4ac286"
+    "sec:orgb27fbc4"
+    "sec:org8092467"
+    "sec:org2f865ea"))
  :latex)
 
