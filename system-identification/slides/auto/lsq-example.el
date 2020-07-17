0ceb4e2f4b5dc723f461f67188a2316c705f9db5
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-17 11:18:28 -0500

    Material for lecture on LS

---
+++ system-identification/slides/auto/lsq-example.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "lsq-example"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "fontenc"
+    "inputenc"
+    "pgf"
+    "tikz"
+    "setspace"
+    "pgfplots"
+    "pgfplotstable"))
+ :latex)
+
