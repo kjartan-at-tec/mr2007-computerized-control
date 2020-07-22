f98cdaa6f300f90b757664b27e433263186f9964
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-22 12:02:05 -0500

    Ready for sysid lecture

--- system-identification/slides/auto/lecture-sysid-2-sp.el
+++ system-identification/slides/auto/lecture-sysid-2-sp.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -43,11 +43,11 @@
     "tdelay"
     "Tcnst")
    (LaTeX-add-labels
-    "sec:org77514cb"
-    "sec:org6e42a51"
-    "sec:org6a036c8"
-    "sec:orgd32a1f9"
-    "sec:orgb8acce3")
+    "sec:org81eb63f"
+    "sec:org790b60b"
+    "sec:org727501a"
+    "sec:org0434284"
+    "sec:org938d752")
    (LaTeX-add-environments
     '("exercise" LaTeX-env-args ["argument"] 0)))
  :latex)
