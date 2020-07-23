71a07f7499bf504cc7d92adbc86fc8002a5dd287
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-07-23 12:06:33 -0500

    Lecture material for sysid

---
+++ system-identification/slides/auto/lecture-sysid-3-sp.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "lecture-sysid-3-sp"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "aspectratio=169")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "khpreamble"
+    "pgfplotstable")
+   (TeX-add-symbols
+    "shift"
+    "diff")
+   (LaTeX-add-labels
+    "sec:org14bb150"
+    "sec:org08be703"
+    "sec:orga698171")
+   (LaTeX-add-environments
+    '("exercise" LaTeX-env-args ["argument"] 0)))
+ :latex)
+
